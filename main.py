23b42479fa220cf81f138108415327d565a2f28a
PrynsTag <201812311@fit.edu.ph>
2021-03-17 04:04:01 +0800

    added file handling and leaderboard.

--- main.py
+++ main.py
@@ -9,24 +9,45 @@ class Game:
             "paper": "scissors",
             "scissors": "rock"
         }
+        self.player_name = None
+        self.leaderBoard = {}
+
+    def greet_player(self):
+        self.player_name = input("Enter your name: ")
+        print(f"Hello, {self.player_name}")
+
+    def read_file(self):
+        with open("rating.txt", "r") as file:
+            for line in file:
+                key, value = line.strip().split(" ")
+                self.leaderBoard[key] = int(value)
 
     def play(self):
-        return self.result(choice(self.choice))
-
-    def result(self, comp_choice):
-        LOSE = "Sorry, but the computer chose {}".format(comp_choice)
-        DRAW = "There is a draw ({})".format(comp_choice)
-        WIN = "Well done. The computer chose {} and failed".format(comp_choice)
+        self.greet_player()
+        self.read_file()
+        # Check if player has existing score, 0 if not
+        SCORE = self.leaderBoard[self.player_name] if self.player_name in self.leaderBoard.keys() else 0
+        LOSE = "Sorry, but the computer chose {}"
+        DRAW = "There is a draw ({})"
+        WIN = "Well done. The computer chose {} and failed"
 
         while True:
             user_choice = input().lower()
+            comp_choice = choice(self.choice)
+
             if comp_choice == user_choice:
-                result = DRAW
+                result = DRAW.format(comp_choice)
+                SCORE += 50
             elif self.user_win[comp_choice] == user_choice:
-                result = WIN
+                result = WIN.format(comp_choice)
+                SCORE += 100
+            elif user_choice == "!rating":
+                result = self.rating()
+            elif user_choice == "!leaderboard":
+                result = self.score_board()
             else:
                 if user_choice in self.user_win:
-                    result = LOSE
+                    result = LOSE.format(comp_choice)
                 else:
                     result = "Invalid Input"
 
@@ -35,7 +56,29 @@ class Game:
                 print("Bye!")
                 break
 
+            self.leaderBoard[self.player_name] = SCORE
             print(result)
+
+    def rock_paper_scissor(self):
+        
+
+    def score_board(self):
+        sorted_dict = dict(sorted(self.leaderBoard.items(),
+                                  key=lambda item: item[1],
+                                  reverse=True))
+        scores, counter = [], 0
+
+        for key, value in sorted_dict.items():
+            counter += 1
+            scores.append(f"{counter}. {key}: {value}")
+
+        return '\n'.join(scores)
+
+    def rating(self):
+        if self.player_name in self.leaderBoard.keys():
+            return f"your rating: {self.leaderBoard[self.player_name]}"
+        else:
+            return f"your rating: 0"
 
 
 if __name__ == '__main__':
